b31ccba46aef34f8d4429f9fc0897cf4919eaf60
Steven Sams <steven.sams@bbc.co.uk>
2015-07-05 23:06:17 +0100

    Added for loop and List

--- src/main/scala/hw.scala
+++ src/main/scala/hw.scala
@@ -1,13 +1,17 @@
 class Hello {
-	def classMethod(text: String) {
-		println("\n" + text)
+	def classMethod(text: String) : String = {
+		return "\n" + text
 	}
 }
 
 object Hi { 
 	def main(args: Array[String]) {
-		print("Hi there!")
+		var hi: String = "Hi there "
+		val names = List("John", "James", "Paul")
 		val c = new Hello
-		c.classMethod("Say hello!")
-	} 
+		for(name <- names) {
+			val  response = c.classMethod("Say hello " + name)
+			println(response)
+		}
+	}
 }
