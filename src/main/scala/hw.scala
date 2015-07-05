dbc3f2e1a03dd40706d7a2b60063156c8efaf8de
Steven Sams <steven.sams@bbc.co.uk>
2015-07-05 22:25:05 +0100

    Using classes / methods

--- src/main/scala/hw.scala
+++ src/main/scala/hw.scala
@@ -1,3 +1,13 @@
+class Hello {
+	def classMethod(text: String) {
+		println("\n" + text)
+	}
+}
+
 object Hi { 
-	def main(args: Array[String]) = print("Hi there!") 
+	def main(args: Array[String]) {
+		print("Hi there!")
+		val c = new Hello
+		c.classMethod("Say hello!")
+	} 
 }
