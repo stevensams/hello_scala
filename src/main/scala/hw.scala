772ecb4955e4574f307b031f9e824ef91ed88f62
Steven Sams <steven.sams@bbc.co.uk>
2015-07-12 18:24:47 +0100

    Using apply function

--- src/main/scala/hw.scala
+++ src/main/scala/hw.scala
@@ -6,27 +6,29 @@ trait Goodbye {
 	val bye: String
 }
 
-class Greeting extends Hello with Goodbye {
-	val hi = "Hello "
-	val bye = "Goodbye "
+class Greeting(hello: String, goodbye: String) extends Hello with Goodbye {
+	val hi = hello
+	val bye = goodbye
 	def hello(text: String) : String = hi + text
 	def goodbye(text: String) : String = bye + text
 }
 
-object Hi { 
+object Hw {
 	def main(args: Array[String]) {
 		// sbt 'run hello' 
 		val output: String = if(args(0) == "hello") {
-			hello
+			Yo()
 		} else {
 			"Unknown argument: " + args(0)
 		}
 		println(output)
 	}
+}
 
-	def hello() : String = {
+object Yo {
+	def apply() : String = {
 		val names = List("John", "James", "Paul", "Roger")
-		val c = new Greeting
+		val c = new Greeting("Hello ", "Goodbye ")
 		var response = ""
 		/*for(name <- names) {
 			response = response + c.hello(name) + "\n"
@@ -35,7 +37,7 @@ object Hi {
 		names.foreach(name => response = response + c.hello(name) + "\n" + c.goodbye(name) + "\n")
 		response
 	}
-
+}
 	/*
 
 	def functionName(argumentName: argumentType) : returnType = {
@@ -45,4 +47,3 @@ object Hi {
 	def functionName(argumentName: argumentType) : returnType = calculation
 
 	*/
-}
